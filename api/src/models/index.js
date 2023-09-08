caa78cb5a250378986fc8ccf8762f14dec7bdc33
hodehoujolive <jolivehodehou7@gmail.com>
2023-09-08 21:20:27 +0100

    Delete main

--- api/src/models/index.js
+++
@@ -1,5 +0,0 @@
-module.exports.Token = require('./token.model');
-module.exports.User = require('./user.model');
-module.exports.Team = require('./team.model');
-module.exports.Player = require('./player.model');
-module.exports.Match = require('./match.model');
--- api/src/services/index.js
+++
@@ -1,7 +0,0 @@
-module.exports.authService = require('./auth.service');
-module.exports.emailService = require('./email.service');
-module.exports.tokenService = require('./token.service');
-module.exports.userService = require('./user.service');
-module.exports.teamService = require('./team.service');
-module.exports.playerService = require('./player.service');
-module.exports.matchService = require('./match.service');
